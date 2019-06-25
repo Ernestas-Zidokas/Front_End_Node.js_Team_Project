30654e97b102a5192382d99987b85bd30fcec7c9
Austinskas <a.austinskas@yahoo.com>
2019-06-25 20:49:26 +0300

    Photo upload

--- frontEnd/js/instaFeed.js
+++ frontEnd/js/instaFeed.js
@@ -34,3 +34,43 @@ window.addEventListener('load', () => {
 //       console.log(err)
 //     })
 // }
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("addPost");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// Upload Image
+
+let input = document.querySelector('input[type="file"]')
+
+let data = new FormData()
+data.append('file', input.files[0])
+data.append('user', 'hubot')
+
+fetch('/avatars', {
+ method: 'POST',
+ body: data
+})
